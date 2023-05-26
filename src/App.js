5d2e85b4e23be8de657e5246a77c588f78275ff5
Universal0090 <idealxorezm@gmail.com>
2023-05-26 22:12:00 +0500

    quiz

--- src/App.js
+++ src/App.js
@@ -4,64 +4,6 @@ import questions from './db';
 import Result from './Result';
 import Game from './Game';
 
-// function Result({ correct }) {
-//   return (
-//     <div className="result">
-//       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-//       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-//       <a href='/'>
-//         <button>Попробовать снова</button>
-//       </a>
-//     </div>
-//   );
-// }
-
-
-// const questions = [
-//   {
-//     title: 'React - это ... ?',
-//     variants: ['библиотека', 'фреймворк', 'приложение'],
-//     correct: 0,
-//   },
-//   {
-//     title: 'Компонент - это ... ',
-//     variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-//     correct: 1,
-//   },
-//   {
-//     title: 'Что такое JSX?',
-//     variants: [
-//       'Это простой HTML',
-//       'Это функция',
-//       'Это тот же HTML, но с возможностью выполнять JS-код',
-//     ],
-//     correct: 2,
-//   },
-// ];
-
-
-// function Game({ state, question, onclickVariant }) {
-//   const persontarget = Math.round((state / questions.length) * 100);
-//   console.log(persontarget);
-
-//   return (
-//     <>
-//       <div className="progress">
-//         <div style={{ width: `${persontarget}%` }} className="progress__inner"></div>
-//       </div>
-//       <h1>{question.title}</h1>
-//       <ul>
-//         {
-//           question.variants.map((text, index) => {
-//             return (
-//               <li onClick={() => onclickVariant(index)} key={index}>{text}</li>
-//             )
-//           })
-//         }
-//       </ul>
-//     </>
-//   );
-// }
 
 function App() {
 
